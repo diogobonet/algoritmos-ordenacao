a44298d7608e1c01640cc033d198ad17ec030ed2
Felipe Vermelho Martins <86678984+FelipeRed@users.noreply.github.com>
2023-11-07 10:11:33 -0300

    Colocando o Array que a professora pediu

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int[] array1 = {3, 2, 6, 5, 1, 7, 4};
+        int[] array1 = {49, 15, 40, 27, 20, 19, 50, 2, 12, 28, 49, 44, 9, 59, 18, 5, 30, 6, 7, 28};
         int[] array2 = Arrays.copyOf(array1, array1.length);
         int[] array3 = Arrays.copyOf(array1, array1.length);
 
